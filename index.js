74b83cce11a524fea5e83dd41a55c3da1142cb38
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-10 14:59:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const mapToNegativize=(sourceArray) => sourceArray.map(ele => ele *-1)
 const mapToNoChange=(sourceArray) => sourceArray.map(ele => ele)
 const mapToDouble=(sourceArray) => sourceArray.map(ele => ele *2)
 const mapToSquare=(sourceArray) => sourceArray.map(ele => ele **2);
-const reduceToTotal=(sourceArray, startingPoint) =>sourceArray.reduce((acc, v)acc+v, startingPoint)
+const reduceToTotal;
 const reduceToAllTrue=(sourceArray)
 const reduceToAnyTrue=(sourceArray)
